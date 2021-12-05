848032875c4999c2e1b0e132e21fdc45cfa7e6b6
Haythem Ben Drissia <hbendrissia@vermeg.com>
2021-12-05 19:23:54 +0100

    Two-way Binding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './products/product-list.components';
 
 @NgModule({
-  imports: [ BrowserModule ], // external modules
+  imports: [ BrowserModule, FormsModule ], // external modules
   declarations: [ AppComponent, ProductListComponent  ], // components
   bootstrap: [ AppComponent ] // startup component of an application
 })
