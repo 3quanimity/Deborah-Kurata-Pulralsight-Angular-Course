a27329f50e1abbae44467cda9d2b7e502528602b
Haythem Ben Drissia <hbendrissia@vermeg.com>
2021-12-06 16:38:35 +0100

    building a custom pipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './products/product-list.components';
+import { ConvertToSpacesPipe } from './shared/convert-to-spaces.pipe';
 
 @NgModule({
-  imports: [ BrowserModule, FormsModule ], // external modules
-  declarations: [ AppComponent, ProductListComponent  ], // components
-  bootstrap: [ AppComponent ] // startup component of an application
+  declarations: [AppComponent, ProductListComponent, ConvertToSpacesPipe], // components
+  imports: [BrowserModule, FormsModule], // external modules
+  bootstrap: [AppComponent], // startup component of an application
 })
-export class AppModule { }
+export class AppModule {}
